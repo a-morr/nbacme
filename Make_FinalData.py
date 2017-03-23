9f766b5612ca2d9e5d25dd2e034dc5e6840a3ab1
Ben Ehlert <ben@ehlert.org>
2017-03-23 13:49:21 -0600

    Generalized rolling average function

--- Make_FinalData.py
+++ Make_FinalData.py
@@ -3,16 +3,17 @@ import pandas as pd
 
 
 
-def RollingAvgPoints(df,teams):
-    df['Roll_Avg5_PTS'] = np.nan
+def RollingAvg(df,teams,column,window):
+    new_col = 'RollAvg_'+str(window)+'_'+column
+    df[new_col] = np.nan
     for team in teams:
         fran_indices = df[(df['fran_id']==team)].index
-        fran_points = pd.rolling_mean(df[(df['fran_id']==team)]['pts'],5)
-        df.ix[fran_indices,'Roll_Avg5_PTS'] = fran_points.tolist()
+        fran_points = pd.rolling_mean(df[(df['fran_id']==team)][column],window)
+        df.ix[fran_indices,new_col] = fran_points.tolist()
 
         fran_indices = df[(df['opp_fran']==team)].index
-        fran_points = pd.rolling_mean(df[(df['opp_fran']==team)]['pts'],5)
-        df.ix[fran_indices,'Roll_Avg5_PTS'] = fran_points.tolist()
+        fran_points = pd.rolling_mean(df[(df['opp_fran']==team)][column],window)
+        df.ix[fran_indices,new_col] = fran_points.tolist()
 
 if __name__ == "__main__":
     # Read in data
@@ -21,7 +22,9 @@ if __name__ == "__main__":
     teams = df['fran_id'].unique()
     
     # Add new columns here
-    RollingAvgPoints(df,teams)
+    colToAdd = ['pts','opp_pts']
+    for col in colToAdd:
+        RollingAvg(df,teams,col,5)
     
     # Save final dataframe
     print df
